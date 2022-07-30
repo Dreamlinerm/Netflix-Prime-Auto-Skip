0c50cde5ef87e1a3b7ad3b6af56207dbaabb27fd
Marvin Alexander Krebber <marvinkrebber@yahoo.de>
2022-07-30 20:46:04 +0200

    localstorage

--- primeskip.js
+++ primeskip.js
@@ -6,53 +6,74 @@ let url = window.location;
 // test url with regex for video
 let isAmazon = /amazon/i.test(url);
 let isVideo = /video/i.test(url);
-// if (isAmazon) {
-//   alert("isAmazon");
-// }
 if (isAmazon && isVideo) {
-  //   alert("isVideo");
+  // localStorage
+  // let localStorage = window.localStorage;
+  // localStorage.setItem("skipVideo", "true");
+  // localStorage.setItem("skipAd", "true");
+  let skipVideos = localStorage.getItem("skipVideo");
+  let skipAds = localStorage.getItem("skipAd");
+  if (skipVideo == null) {
+    localStorage.setItem("skipVideo", "true");
+  }
+  if (skipAd == null) {
+    localStorage.setItem("skipAd", "true");
+  }
+  console.log("skipVideo: ", skipVideos, "skipAd: ", skipAds);
   setInterval(function () {
     // skip intro in video
-    skipVideo();
+    if (skipVideos) {
+      skipVideo();
+    }
     // skip ads
-    skipAd();
+    if (skipAds) {
+      // does not currently work properly
+      // skipAd();
+    }
   }, 500); //1000 = 1000ms = 1s
-  let Tags;
   let SkipButtonClass = new RegExp("skipelement", "i");
   let SkipButtonText = new RegExp("Skip", "i");
   let AdSkipClass = new RegExp("fu4rd6c f1cw2swo", "i");
   //   document.getElementsByClassName("fu4rd6c f1cw2swo")[0].click();", "i");
-  let found;
-  function skipVideo() {
-    Tags = document.getElementsByTagName("button");
+
+  async function skipVideo() {
+    let IntroTags = document.getElementsByTagName("button");
+    let foundIntro;
     // for (var i = 0; i < Tags.length; i++) {
-    for (const Tag of Tags) {
+    for (const Tag of IntroTags) {
       // Search for textcontent instead of classname
       //  if (SkipButtonText.test(Tag.textContent)) {
       if (
         SkipButtonClass.test(Tag.classList) ||
         SkipButtonText.test(Tag.textContent)
       ) {
-        found = Tag;
+        foundIntro = Tag;
         break;
       }
     }
-    found.click();
+    if (foundIntro) {
+      console.log("skipped Intro");
+      foundIntro.click();
+    }
   }
-  function skipAd() {
-    Tags = document.getElementsByTagName("div");
+  async function skipAd() {
+    let AdTags = document.getElementsByTagName("div");
+    let foundAd;
     // for (var i = 0; i < Tags.length; i++) {
-    for (const Tag of Tags) {
+    for (const Tag of AdTags) {
       if (
         AdSkipClass.test(Tag.classList) ||
         SkipButtonText.test(Tag.textContent)
       ) {
-        found = Tag;
+        foundAd = Tag;
         break;
       }
     }
     // click div element
-    found.click();
+    if (foundAd) {
+      console.log("skipped Ad");
+      foundAd.click();
+    }
   }
 }
 // for(var sec=2; sec>0; sec--){
