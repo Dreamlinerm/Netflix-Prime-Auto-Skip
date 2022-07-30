ebb64564e1682e533b772de889f8f80a9e02f6e3
Marvin Alexander Krebber <marvinkrebber@yahoo.de>
2022-07-30 17:11:19 +0200

    auto skips intros

--- primeskip.js
+++ primeskip.js
@@ -1,29 +1,25 @@
-/*
-Just draw a border round the document.body.
-*/
-document.body.style.border = "5px solid red";
 var url = window.location;
 // var path = window.location.pathname;
 // test url with regex for video
 var isVideo = /video|Safari/i.test(url);
 // alert(url + "\n" + isVideo);
-
-var aTags = document.getElementsByTagName("button");
-var searchText = new RegExp("Skip", "i");
-var found;
-
-// for (var i = 0; i < aTags.length; i++) {
-//   if (searchText.test(aTags[i].textContent)) {
-//     found = aTags[i];
-//     break;
-//   }
-// }
-for (const Tag of aTags) {
-  if (searchText.test(Tag.textContent)) {
-    found = Tag;
-    break;
+if (isVideo) {
+  alert("test1");
+  setInterval(function () {
+    // execute script
+    skipVideo();
+  }, 1000); //1000 = 1000ms = 1s
+  function skipVideo() {
+    var aTags = document.getElementsByTagName("button");
+    var searchText = new RegExp("Skip", "i");
+    var found;
+    // for (var i = 0; i < aTags.length; i++) {
+    for (const Tag of aTags) {
+      if (searchText.test(Tag.textContent)) {
+        found = Tag;
+        break;
+      }
+    }
+    found.click();
   }
 }
-found.click();
-alert(found.textContent);
-// Use `found`.
