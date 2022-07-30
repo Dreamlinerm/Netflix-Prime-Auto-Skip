2843b46a2c0d9835be1c2b6ee581d7d2cd8a8854
Marvin Alexander Krebber <marvinkrebber@yahoo.de>
2022-07-30 18:15:22 +0200

    skip ad not working right now

--- primeskip.js
+++ primeskip.js
@@ -1,24 +1,46 @@
+// old matches but because of domain ending not matching, it is not used
+// match all amazon pages
+// *://*.amazon.de/*/video/*
+
 let url = window.location;
 // test url with regex for video
+let isAmazon = /amazon/i.test(url);
 let isVideo = /video/i.test(url);
-if (isVideo) {
-  //   alert("isVideo");
+// if (isAmazon) {
+//   alert("isAmazon");
+// }
+if (isAmazon && isVideo) {
+  //alert("isVideo");
   setInterval(function () {
     // execute script
+    // skip intro in video
     skipVideo();
+    //skipAd();
   }, 500); //1000 = 1000ms = 1s
-  let aTags;
+  let Tags;
   let searchText = new RegExp("Skip", "i");
   let found;
   function skipVideo() {
-    aTags = document.getElementsByTagName("button");
-    // for (var i = 0; i < aTags.length; i++) {
-    for (const Tag of aTags) {
+    Tags = document.getElementsByTagName("button");
+    // for (var i = 0; i < Tags.length; i++) {
+    for (const Tag of Tags) {
       if (searchText.test(Tag.textContent)) {
         found = Tag;
         break;
       }
     }
+    found.click();
+  }
+  function skipAd() {
+    Tags = document.getElementsByTagName("div");
+    // for (var i = 0; i < Tags.length; i++) {
+    for (const Tag of Tags) {
+      if (searchText.test(Tag.textContent)) {
+        found = Tag;
+        break;
+      }
+    }
+    // click div element
     found.click();
   }
 }
