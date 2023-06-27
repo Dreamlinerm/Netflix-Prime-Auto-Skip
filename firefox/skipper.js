f4c3094b9eef43f87f52cc7d40dd64fdbf817e19
Marvin Alexander Krebber <marvinkrebber@yahoo.de>
2023-06-27 20:47:38 +0200

    intro

--- firefox/skipper.js
+++ firefox/skipper.js
@@ -159,7 +159,9 @@ if (isPrimeVideo || isNetflix || isDisney) {
   function Disney_Intro(mutations, observer) {
     // intro star wars andor Season 1 episode 2
     // Recap Criminal Minds Season 1 Episode 2
-    let button = document.querySelector(".skip__button");
+    let button;
+    if (isDisney) button = document.querySelector(".skip__button");
+    else button = document.evaluate("//span[contains(., 'Skip')]", document, null, XPathResult.ANY_TYPE, null).iterateNext().parentElement;
     if (button) {
       let video = document.querySelector("video");
       const time = video.currentTime;
@@ -175,7 +177,7 @@ if (isPrimeVideo || isNetflix || isDisney) {
   function Disney_Credits(mutations, observer) {
     let button;
     if (isDisney) button = document.querySelector('[data-gv2elementkey="playNext"]');
-    else button = document.evaluate("//span[contains(., 'Next Episode')]", document, null, XPathResult.ANY_TYPE, null).iterateNext().parentElement;
+    else button = document.evaluate("//span[contains(., 'Next')]", document, null, XPathResult.ANY_TYPE, null).iterateNext().parentElement;
     if (button) {
       // only skip if the next video is the next episode of a series (there is a timer)
       let time;
