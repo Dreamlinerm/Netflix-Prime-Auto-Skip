29a72c5436afd73d1e8b531d1428737c455e3a14
Marvin Alexander Krebber <marvinkrebber@yahoo.de>
2023-08-16 01:15:33 +0200

    test

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
 
 
 ffOptions = Options()
@@ -12,4 +13,16 @@ driver = webdriver.Firefox(options=ffOptions)
 extension_path = "NetflixPrime@Autoskip.io.xpi"
 driver.install_addon(extension_path, temporary=True)
 driver.implicitly_wait(500)
+
+## Netflix tests
+driver.get("https://www.netflix.com/watch/80011385")
+# click on profile-icon class
+# driver.find_element(by=By.CLASS_NAME, value="profile-icon").click()
+# click on video element
+driver.find_element(by=By.CLASS_NAME, value="watch-video--autoplay-blocked").click()
+
+video = driver.find_element(by=By.TAG_NAME, value="video")
+print(video)
+video.currentTime = 33  # does not work on netflix
+
 driver.quit()
