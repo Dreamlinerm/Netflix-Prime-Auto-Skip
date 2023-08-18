14679f6908db97b938c3d131c2b18684a9d1b28f
Marvin Alexander Krebber <marvinkrebber@yahoo.de>
2023-08-18 22:43:16 +0200

    function searchTitle

--- test.js
+++ test.js
@@ -1,28 +1,59 @@
-title = "How I Met Your Mother";
-title = title.replaceAll(" ", "%20");
-const url = "https://moviesdatabase.p.rapidapi.com/titles/search/title/" + title + "?exact=true&info=base_info";
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "3798a400e8mshced4564d240d851p1b4f85jsn717e097775dd",
-    "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
-  },
-};
+// let title = "How I Met Your Mother";
+// title = title.replaceAll(" ", "%20");
+// const url = "https://moviesdatabase.p.rapidapi.com/titles/search/title/" + title + "?exact=true&info=base_info";
+// const options = {
+//   method: "GET",
+//   headers: {
+//     "X-RapidAPI-Key": "3798a400e8mshced4564d240d851p1b4f85jsn717e097775dd",
+//     "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
+//   },
+// };
 
-try {
-  fetch(url, options)
+// try {
+//   fetch(url, options)
+//     .then((response) => {
+//       if (!response.ok) {
+//         throw new Error("Network response was not ok");
+//       }
+//       return response.text();
+//     })
+//     .then((text) => {
+//       console.log(text);
+//     })
+//     .catch((error) => {
+//       console.error(error);
+//     });
+// } catch (error) {
+//   console.error(error);
+// }
+
+function searchTitle(title) {
+  title = title.replaceAll(" ", "%20");
+  const url = "https://moviesdatabase.p.rapidapi.com/titles/search/title/" + title + "?exact=true&info=base_info";
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "3798a400e8mshced4564d240d851p1b4f85jsn717e097775dd",
+      "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
+    },
+  };
+
+  return fetch(url, options)
     .then((response) => {
       if (!response.ok) {
         throw new Error("Network response was not ok");
       }
       return response.text();
     })
-    .then((text) => {
-      console.log(text);
-    })
     .catch((error) => {
       console.error(error);
     });
-} catch (error) {
-  console.error(error);
 }
+
+searchTitle("How I Met Your Mother")
+  .then((text) => {
+    console.log(text);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
