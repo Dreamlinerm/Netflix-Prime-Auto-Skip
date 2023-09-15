1e8580dfe1e7604719a7d3f83ae4a87272e209d3
Marvin Alexander Krebber <marvinkrebber@yahoo.de>
2023-09-15 10:29:59 +0200

    justWatch test positive

--- justWatchTest.js
+++ justWatchTest.js
@@ -3,11 +3,15 @@ async function getMovieInfo(movieTitle) {
   const url = `https://apis.justwatch.com/content/titles/${locale}/popular?language=en&body={"page_size":1,"page":1,"query":"${movieTitle}","content_types":["show","movie"]}`;
   const response = await fetch(encodeURI(url));
   const data = await response.json();
-  console.log(data);
+  //   console.log(data);
   const loc = data.items[0].full_path;
   const opts = { url: `https://www.justwatch.com${loc}` };
-  console.log(opts);
+  //   console.log(opts);
+  //   console.log(data.items[0].offers.filter((x) => x.monetization_type != "flatrate" && x.monetization_type != "rent" && x.monetization_type != "buy"));
+  console.log(data.items[0].offers.filter((x) => x.monetization_type == "flatrate" && (x.package_short_name == "amp" || x.package_short_name == "nfx" || x.package_short_name == "dnp")));
 }
 
-let movieTitle = "Three Thousand Years of Longing";
+// let movieTitle = "The Thousand Years of Longing";
+// let movieTitle = "Breaking Bad";
+let movieTitle = "Elemental";
 getMovieInfo(movieTitle);
