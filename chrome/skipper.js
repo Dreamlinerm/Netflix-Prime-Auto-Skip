82a463c889cb45c68adc3303158ba69e50abcac4
Marvin Alexander Krebber <marvinkrebber@yahoo.de>
2023-10-26 17:37:48 +0200

    version 60

--- chrome/skipper.js
+++ chrome/skipper.js
@@ -23,7 +23,7 @@ let isHotstar = /hotstar/i.test(hostname);
 
 let isEdge = /edg/i.test(ua);
 let isFirefox = /firefox/i.test(ua);
-const version = "1.0.59";
+const version = "1.0.60";
 if (isPrimeVideo || isNetflix || isDisney || isHotstar) {
   // global variables in localStorage
   const defaultSettings = {
