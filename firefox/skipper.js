a08e97d2c0d9f6e18b34b1944bdfc04552c1dc75
Dreamliner <marvinkrebber@yahoo.de>
2023-12-30 19:00:23 +0100

    Add Epilepsy option (#76)
    * epilepsy option
    
    * Netflix epilepsy adskip
    
    * chrome
    
    * chrome translations

--- firefox/skipper.js
+++ firefox/skipper.js
@@ -35,7 +35,7 @@ if (isPrimeVideo || isNetflix || isDisney || isHotstar || isCrunchyroll) {
       Netflix: { skipIntro: true, skipRecap: true, skipCredits: true, watchCredits: false, skipBlocked: true, skipAd: true, speedSlider: true, profile: true, showRating: true },
       Disney: { skipIntro: true, skipCredits: true, watchCredits: false, speedSlider: true, showRating: true },
       Crunchyroll: { skipIntro: true, speedSlider: true, releaseCalendar: true },
-      Video: { playOnFullScreen: true },
+      Video: { playOnFullScreen: true, epilepsy: false },
       Statistics: { AmazonAdTimeSkipped: 0, NetflixAdTimeSkipped: 0, IntroTimeSkipped: 0, RecapTimeSkipped: 0, SegmentsSkipped: 0 },
       General: { profileName: null, profilePicture: null, sliderSteps: 1, sliderMin: 5, sliderMax: 20, filterDub: true, filterQueued: true },
     },
@@ -515,6 +515,7 @@ if (isPrimeVideo || isNetflix || isDisney || isHotstar || isCrunchyroll) {
           log("Ad skipped, length:", adLength, "s");
           settings.Statistics.NetflixAdTimeSkipped += adLength;
           increaseBadge();
+          if (settings.Video.epilepsy) video.style.opacity = 0;
           video.playbackRate = playBackRate;
           lastAdTimeText = adLength;
         } else if (adLength > 2 && video.playbackRate < 2) {
@@ -527,6 +528,7 @@ if (isPrimeVideo || isNetflix || isDisney || isHotstar || isCrunchyroll) {
           // videospeed is speedSlider value
           video.playbackRate = videoSpeed;
           lastAdTimeText = 0;
+          if (settings.Video.epilepsy) video.style.opacity = 1;
         }
       }
     }, 100);
