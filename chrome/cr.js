250f4d9dfb273ed874371a4456eac9c8896ca4b8
Marvin Alexander Krebber <marvinkrebber@yahoo.de>
2024-01-28 14:32:40 +0100

    1.0.84

--- chrome/cr.js
+++ chrome/cr.js
@@ -43,7 +43,7 @@ const defaultSettings = {
   },
 };
 let settings = defaultSettings.settings;
-const version = "1.0.83";
+const version = "1.0.84";
 chrome.storage.sync.get("settings", function (result) {
   console.log(
     "%cNetflix%c/%cPrime%c Auto-Skip",
