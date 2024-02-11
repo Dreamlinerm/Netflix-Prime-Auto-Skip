f81b6b5bb2737921e64e9e62a6fcf8328a7741a4
Marvin Alexander Krebber <marvinkrebber@yahoo.de>
2024-02-11 09:53:11 +0100

    spellings

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
       env: {
         node: true,
       },
-      files: [".eslintrc.{js,cjs}"],
+      files: [".eslintrc.{js,cjs,html}"],
       parserOptions: {
         sourceType: "script",
       },
