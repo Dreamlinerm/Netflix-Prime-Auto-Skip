6ac12b7639055a2ad283b0f18924be7c5f4c8ace
Marvin Alexander Krebber <marvinkrebber@yahoo.de>
2024-03-08 10:53:18 +0100

    all files are replaced

--- copyFtoC.js
+++ copyFtoC.js
@@ -2,8 +2,37 @@
 const fs = require("fs");
 const path = require("path");
 
+// Check if 'chrome' folder exists, if not, create it
+if (!fs.existsSync("chrome")) {
+  fs.mkdirSync("chrome");
+}
+
 // Read the files where 'browser' is used
-const browserFiles = ["skipper.js", "cr.js", path.join("popup", "settings.js"), path.join("popup", "settings.css")];
+const browserFiles = [
+  "skipper.js",
+  "cr.js",
+  path.join("popup", "settings.js"),
+  path.join("popup", "settings.css"),
+  path.join("popup", "popup.html"),
+  path.join("popup", "settings.html"),
+
+  path.join("_locales", path.join("de", "messages.json")),
+  path.join("_locales", path.join("en", "messages.json")),
+  path.join("_locales", path.join("mk", "messages.json")),
+  path.join("_locales", path.join("pt_BR", "messages.json")),
+];
+const replaces = [
+  [/browser([^-])/g, "chrome$1"],
+  [
+    "https://addons.mozilla.org/firefox/addon/netflix-prime-auto-skip/",
+    "https://chrome.google.com/webstore/detail/netflixprime-auto-skip/akaimhgappllmlkadblbdknhbfghdgle",
+  ],
+  [
+    "https://img.shields.io/amo/stars/NetflixPrime@Autoskip.io?color=e60010",
+    "https://img.shields.io/chrome-web-store/stars/akaimhgappllmlkadblbdknhbfghdgle?color=e60010",
+  ],
+  ["Streaming enhanced: Netflix Disney+ Prime Video", "Streaming enhanced Netflix Disney Prime Video"],
+];
 for (let file of browserFiles) {
   console.log(path.join("firefox", file) + ":");
   fs.readFile(path.join("firefox", file), "utf8", function (err, data) {
@@ -12,34 +41,10 @@ for (let file of browserFiles) {
     }
 
     // Replace 'browser' with 'chrome'
-    let result = data.replace(/browser([^-])/g, "chrome$1");
-
-    // Write the updated content to a new file in the 'chrome' folder
-    fs.writeFile(path.join("chrome", file), result, "utf8", function (err) {
-      if (err) return console.log(err);
-    });
-  });
-}
-
-// Read the files where html is used
-const htmlFiles = [path.join("popup", "popup.html"), path.join("popup", "settings.html")];
-for (let file of htmlFiles) {
-  console.log(path.join("firefox", file) + ":");
-  fs.readFile(path.join("firefox", file), "utf8", function (err, data) {
-    if (err) {
-      return console.log(err);
+    let result = data;
+    for (let replace of replaces) {
+      result = result.replace(replace[0], replace[1]);
     }
-
-    // Replace 'browser' with 'chrome'
-    let result = data
-      .replace(
-        "https://addons.mozilla.org/firefox/addon/netflix-prime-auto-skip/",
-        "https://chrome.google.com/webstore/detail/netflixprime-auto-skip/akaimhgappllmlkadblbdknhbfghdgle"
-      )
-      .replace(
-        "https://img.shields.io/amo/stars/NetflixPrime@Autoskip.io?color=e60010",
-        "https://img.shields.io/chrome-web-store/stars/akaimhgappllmlkadblbdknhbfghdgle?color=e60010"
-      );
 
     // Write the updated content to a new file in the 'chrome' folder
     fs.writeFile(path.join("chrome", file), result, "utf8", function (err) {
