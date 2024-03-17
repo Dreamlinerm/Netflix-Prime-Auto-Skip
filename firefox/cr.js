be4f8156dff2cc2f0a70c6491c3e02566c9e8497
Marvin Alexander Krebber <marvinkrebber@yahoo.de>
2024-03-17 21:41:37 +0100

    v.1.0.96

--- firefox/cr.js
+++ firefox/cr.js
@@ -44,7 +44,7 @@ const defaultSettings = {
   },
 };
 let settings = defaultSettings.settings;
-const version = "1.0.95";
+const version = "1.0.96";
 browser.storage.sync.get("settings", function (result) {
   console.log(
     "%cNetflix%c/%cPrime%c Auto-Skip",
