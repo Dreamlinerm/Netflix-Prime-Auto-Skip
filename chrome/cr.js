c43d51d8fc712120dd29a03e8b3e7ac03d14b02c
Marvin Alexander Krebber <marvinkrebber@yahoo.de>
2024-05-04 20:40:01 +0200

    version 1.1.8

--- chrome/cr.js
+++ chrome/cr.js
@@ -54,7 +54,7 @@ const defaultSettings = {
   },
 };
 let settings = defaultSettings.settings;
-const version = "1.1.7";
+const version = "1.1.8";
 chrome.storage.sync.get("settings", function (result) {
   console.log(
     "%cNetflix%c/%cPrime%c Auto-Skip",
--- chrome/skipper.js
+++ chrome/skipper.js
@@ -29,7 +29,7 @@ const isMobile = /mobile|streamingEnhanced/i.test(ua);
 const isEdge = /edg/i.test(ua);
 // const isFirefox = /firefox/i.test(ua);
 // const isChrome = /chrome/i.test(ua);
-const version = "1.1.7";
+const version = "1.1.8";
 if (isPrimeVideo || isNetflix || isDisney || isHotstar || isCrunchyroll || isHBO) {
   /* eslint-env root:true */
   // global variables in localStorage
@@ -449,10 +449,8 @@ if (isPrimeVideo || isNetflix || isDisney || isHotstar || isCrunchyroll || isHBO
       // only skip if the next video is the next episode of a series (there is a timer)
       let time;
       if (isDisney) time = /\d+/.exec(button.textContent)?.[0];
-      if (
-        (isHotstar && !document.evaluate("//span[contains(., 'My Space')]", document, null, XPathResult.ANY_TYPE, null)?.iterateNext()) ||
-        (time && lastAdTimeText != time)
-      ) {
+      else time = parseInt(document.querySelector("video")?.currentTime);
+      if (time && lastAdTimeText != time) {
         const videoFullscreen = document.fullscreenElement !== null;
         lastAdTimeText = time;
         if (settings.Disney?.skipCredits) {
