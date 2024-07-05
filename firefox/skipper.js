071e2ca6c6a2fb73a25a7de88733684b8bbd88b1
Marvin Alexander Krebber <marvinkrebber@yahoo.de>
2024-07-06 01:51:03 +0200

    // TODO: translate

--- firefox/skipper.js
+++ firefox/skipper.js
@@ -820,6 +820,7 @@ if (isPrimeVideo || isNetflix || isDisney || isHotstar || isCrunchyroll || isHBO
       button.style = "padding: 0px 22px; line-height: normal; min-width: 0px;z-index:999;pointer-events:all;";
       button.setAttribute("class", "fqye4e3 f1ly7q5u fk9c3ap fz9ydgy f1xrlb00 f1hy0e6n fgbpje3 f1uteees f1h2a8xb  f1cg7427 fiqc9rt fg426ew f1ekwadg");
       button.setAttribute("data-uia", "reverse-button");
+      // TODO: translate
       button.textContent = "Watch skipped ?";
       document.querySelector(".atvwebplayersdk-action-buttons").appendChild(button);
       let buttonInHTML = document.querySelector('[data-uia="reverse-button"]');
