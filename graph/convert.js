aeb0316442f5119867aaeea316068fc5c48111b9
Marvin Alexander Krebber <marvinkrebber@yahoo.de>
2024-08-06 08:41:39 +0200

    added chrome to charts

---
+++ graph/convert.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const path = require("path");
+function convertCsv(name) {
+  let file = fs
+    .readFileSync(path.join(__dirname, name + ".csv"), "utf-8")
+    .split("\n")
+    .map((line) => line.split(","))
+    .map((line) => {
+      return {
+        date: line[0],
+        count: parseInt(line[1]),
+      };
+    });
+  // remove all entries until count is greater than 0
+  while (file[0].count === 0) {
+    file.shift();
+  }
+  // delete last two entries
+  file.pop();
+  file.pop();
+  console.log(file);
+  fs.writeFileSync(path.join(__dirname, name + ".js"), "let " + name + " = " + JSON.stringify(file));
+}
+convertCsv("chromeUsers");
+convertCsv("chromeDownloads");
