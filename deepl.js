f0809d0be6e050db59cd5547fa7b127ffe961e99
Marvin Alexander Krebber <marvinkrebber@yahoo.de>
2024-08-25 13:53:26 +0200

    finished translationapi

--- deepl.js
+++ deepl.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const { exec } = require("child_process");
 
-exec("jsontt Publish/translate.json -m google2 -n t -fb yes -cl 3 -f EN -t DE FR ES PT IT JA PL SV ZH KO TR", (error, stdout, stderr) => {
+exec("jsontt Publish/translate.json -m deepl -n deepl -fb yes -cl 3 -f EN -t DE FR ES PT IT JA PL SV ZH KO TR", (error, stdout, stderr) => {
   if (error) {
     console.error(`Error: ${error.message}`);
     return;
