55bca335d5cdef8bc74ebb469f417f8aac3c37f9
Marvin Alexander Krebber <marvinkrebber@yahoo.de>
2024-10-20 11:17:18 +0200

    onefile

--- hourstxtToJSON.js
+++ hourstxtToJSON.js
@@ -5,10 +5,10 @@ const path = require("path");
 
 // open AuthorHours.txt and write time into JSON file
 
-const hours = fs.readFileSync("AuthorHours.txt", "utf8");
+const hours = fs.readFileSync("authorHours.json", "utf8");
 console.log(hours.split("\n")[0].split(": ")[1]);
 const json = { time: hours.split("\n")[0].split(": ")[1] };
 
-fs.writeFile("AuthorHours.json", JSON.stringify(json), "utf8", function (err) {
+fs.writeFile("authorHours.json", JSON.stringify(json), "utf8", function (err) {
   if (err) return console.log(err);
 });
