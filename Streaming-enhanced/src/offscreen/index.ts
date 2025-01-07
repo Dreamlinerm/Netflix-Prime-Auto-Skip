86e4e8689780c5b7f00bf3eddb838f45dcd6e500
Marvin Alexander Krebber <marvinkrebber@yahoo.de>
2025-01-07 23:23:00 +0100

    rm offscreen

--- Streaming-enhanced/src/offscreen/index.ts
+++
@@ -1,9 +0,0 @@
-// redirect logs to background script
-window.console.info = (...data) => {
-  chrome.runtime.sendMessage({
-    type: "CONSOLE_LOG",
-    data,
-  })
-}
-
-console.info("console log from offscreen document")
