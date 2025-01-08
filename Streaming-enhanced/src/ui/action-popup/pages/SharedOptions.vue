929005a395ca4612e91a6dcb2c35f52e7122e569
Marvin Alexander Krebber <marvinkrebber@yahoo.de>
2025-01-08 23:21:15 +0100

    SharedOptions

--- Streaming-enhanced/src/ui/action-popup/pages/SharedOptions.vue
+++ Streaming-enhanced/src/ui/action-popup/pages/SharedOptions.vue
@@ -1,8 +1,61 @@
 <template>
   <div class="line flex">
-    <p>IntroSwitch</p>
+    <p>{{ $t("IntroSwitch") }}</p>
     <Switch v-model="settings.Amazon.skipAd"></Switch>
   </div>
+  <div class="line flex">
+    <p>{{ $t("IntroSwitch") }}</p>
+    <Switch v-model="settings.Amazon.skipIntro"></Switch>
+  </div>
+  <hr />
+  <div class="line flex">
+    <p>{{ $t("CreditsSwitch") }}</p>
+    <Switch v-model="settings.Amazon.skipCredits"></Switch>
+  </div>
+  <div class="line flex">
+    <p>{{ $t("WatchCreditsSwitch") }}</p>
+    <Switch v-model="settings.Amazon.watchCredits"></Switch>
+  </div>
+  <hr />
+  <div class="line flex">
+    <p>{{ $t("AdsSwitch") }}</p>
+    #VideoAds
+    <Switch v-model="settings.Amazon.skipIntro"></Switch>
+  </div>
+  <div class="line flex">
+    <p>{{ $t("SliderSwitch") }}</p>
+    <Switch v-model="settings.Amazon.speedSlider"></Switch>
+  </div>
+  <div class="line flex">
+    <p>{{ $t("FullscreenSwitch") }}</p>
+    <Switch v-model="settings.Video.playOnFullScreen"></Switch>
+  </div>
+  <div class="line flex">
+    <p>{{ $t("DoubleClickSwitch") }}</p>
+    <Switch v-model="settings.Video.doubleClick"></Switch>
+  </div>
+  <div class="line flex">
+    <p>{{ $t("scrollVolumeSwitch") }}</p>
+    <Switch v-model="settings.Video.scrollVolume"></Switch>
+  </div>
+  <div class="line flex">
+    <p>{{ $t("RatingSwitch") }}</p>
+    <Switch v-model="settings.Amazon.showRating"></Switch>
+  </div>
+  <div class="line flex">
+    <p>{{ $t("ShowYearSwitch") }}</p>
+    <Switch v-model="settings.Video.showYear"></Switch>
+  </div>
+  <a
+    href="https://www.themoviedb.org"
+    target="_blank"
+    class="line flex"
+  >
+    <img
+      src="@assets/TMDB.svg"
+      alt="TMDB"
+    />
+  </a>
 </template>
 <script setup lang="ts">
 const optionsStore = useOptionsStore()
