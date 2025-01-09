4019b3bb3b44db355b94ca54a2051dd129b230f1
Marvin Alexander Krebber <marvinkrebber@yahoo.de>
2025-01-09 09:08:20 +0100

    Netflix

--- Streaming-enhanced/src/ui/action-popup/pages/Netflix.vue
+++ Streaming-enhanced/src/ui/action-popup/pages/Netflix.vue
@@ -5,33 +5,40 @@
 		</h2>
 	</div>
 	<div class="line flex">
-		<p>{{ $t("SelfAdsSwitch") }}</p>
+		<p>{{ $t("RecapSwitch") }}</p>
 		<Switch
-			v-model="settings.Amazon.skipAd"
+			v-model="settings.Netflix.skipRecap"
 			class="ml-auto"
 		></Switch>
 	</div>
 	<div class="line flex">
-		<p>{{ $t("PaidContentSwitch") }}</p>
+		<p>{{ $t("InactivitySwitch") }}</p>
 		<Switch
-			v-model="settings.Amazon.filterPaid"
+			v-model="settings.Netflix.skipBlocked"
 			class="ml-auto"
 		></Switch>
 	</div>
 	<div class="line flex">
-		<p>{{ $t("ContinuePositionSwitch") }}</p>
+		<p>{{ $t("ProfileSwitch") }}</p>
 		<Switch
-			v-model="settings.Amazon.continuePosition"
+			v-model="settings.Netflix.profile"
 			class="ml-auto"
 		></Switch>
 	</div>
-	<div class="line flex">
-		<p>{{ $t("XraySwitch") }}</p>
-		<Switch
-			v-model="settings.Amazon.xray"
-			class="ml-auto"
-		></Switch>
-	</div>
+	<!-- <div class="line flex">
+			<p>{{ $t("User") }}</p>
+			<p
+				id="profileName"
+				style="text-transform: capitalize"
+			></p>
+			<img
+				id="profilePicture"
+				class="hidden"
+				style="margin-left: auto; height: 40px; border-radius: 4px; margin-right: 8px"
+				alt="profile Picture"
+				src="data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs="
+			/>
+		</div> -->
 </template>
 <script setup lang="ts">
 const optionsStore = useOptionsStore()
