821dce03fa81b665276a55cb7417196866aa82f3
Marvin Alexander Krebber <marvinkrebber@yahoo.de>
2025-01-09 12:24:13 +0100

    tabs api

--- Streaming-enhanced/src/ui/action-popup/app.vue
+++ Streaming-enhanced/src/ui/action-popup/app.vue
@@ -1,5 +1,21 @@
 <script setup lang="ts">
 import { Notivue, Notification } from "notivue"
+// if on streaming page open settings for page
+const query = { active: true, currentWindow: true }
+function callback(tabs: chrome.tabs.Tab[]) {
+	const currentUrl = tabs?.[0]?.url || ""
+	const isPrimeVideo = /.amazon.|.primevideo./i.test(currentUrl)
+	const isNetflix = /.netflix./i.test(currentUrl)
+	const isDisney = /.disneyplus.|.starplus.|.hotstar./i.test(currentUrl)
+	const isCrunchyroll = /.crunchyroll./i.test(currentUrl)
+	// const isHBO = /max/i.test(currentUrl);
+	if (isPrimeVideo) router.push("/action-popup/Amazon")
+	else if (isNetflix) router.push("/action-popup/Netflix")
+	else if (isDisney) router.push("/action-popup/Disney")
+	else if (isCrunchyroll) router.push("/action-popup/Crunchyroll")
+	// else if (isHBO) Menu("HBO");
+}
+chrome.tabs.query(query, callback)
 </script>
 
 <template>
