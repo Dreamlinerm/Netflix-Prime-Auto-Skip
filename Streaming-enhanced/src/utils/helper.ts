c1a23f35b174b452c43cc48e0ff6d5e73caae0fe
Marvin Alexander Krebber <marvinkrebber@yahoo.de>
2025-01-10 10:56:19 +0100

    increase the badge count

--- Streaming-enhanced/src/utils/helper.ts
+++ Streaming-enhanced/src/utils/helper.ts
@@ -3,28 +3,25 @@ import { useOptionsStore } from "@/stores/options.store"
 const pinia = createPinia()
 
 // Global Variables
-
 // Use the store
 export const optionsStore = useOptionsStore(pinia)
-
 export const date = new Date()
-// ...args
+const { settings } = storeToRefs(optionsStore)
+
+// Functions
+// custom log function
 export async function log(...args: any[]) {
 	console.log(date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + date.getMilliseconds(), ...args)
 }
 
+// increase the badge count
 export async function increaseBadge() {
-	settings.Statistics.SegmentsSkipped++
-	try {
-		chrome.storage.sync.set({ settings })
-		chrome.runtime.sendMessage({
-			type: "increaseBadge",
-		})
-	} catch (error) {
-		log(error)
-	}
+	settings.value.Statistics.SegmentsSkipped++
+	chrome.runtime.sendMessage({
+		type: "increaseBadge",
+	})
 }
-
+// checks if the store got the data from the storage
 export async function checkStoreReady(setting: Ref<any>) {
 	return new Promise((resolve) => {
 		const readyStateCheckInterval = setInterval(function () {
