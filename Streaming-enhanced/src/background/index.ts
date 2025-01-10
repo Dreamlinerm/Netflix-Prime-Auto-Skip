40cee3838e377e9aff3183f2f8550afba3ba40e2
Marvin Alexander Krebber <marvinkrebber@yahoo.de>
2025-01-10 17:53:03 +0100

    background

--- Streaming-enhanced/src/background/index.ts
+++ Streaming-enhanced/src/background/index.ts
@@ -37,13 +37,102 @@ self.onerror = function (message, source, lineno, colno, error) {
 }
 
 console.info("hello world from background")
-import { onMessage } from "webext-bridge/background"
-
-onMessage("LOG", runAction)
-async function runAction({ data }: { data: any }) {
-	// process data
-	console.info(...data)
-	// return data
-	return {}
+import { onMessage, sendMessage } from "webext-bridge/background"
+import { log, optionsStore, checkStoreReady } from "@/utils/helper"
+log("background loaded")
+const { settings } = storeToRefs(optionsStore)
+const Badges: { [key: string]: string | number } = {}
+const isMobile = /Android/i.test(navigator.userAgent)
+// Increases Badge by 1
+async function increaseBadge(tabId: number) {
+	if (Badges?.[tabId] === undefined || typeof Badges[tabId] !== "number") {
+		Badges[tabId] = 0
+	}
+	Badges[tabId]++
+	browser.browserAction.setBadgeText({ text: Badges[tabId].toString(), tabId })
 }
-export {}
+// Set Badge to a specific value
+async function setBadgeText(text: string, tabId: number) {
+	Badges[tabId] = text
+	browser.browserAction.setBadgeText({ text, tabId })
+}
+
+// onMessage
+onMessage("fetch", async (message: { sender: any; data: { url: string } }) => {
+	const { sender, data } = message
+	fetch(data.url, {
+		method: "GET",
+		headers: {
+			accept: "application/json",
+			Authorization:
+				"Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5OWQyMWUxMmYzNjU1MjM4NzdhNTAwODVhMmVjYThiZiIsInN1YiI6IjY1M2E3Mjg3MjgxMWExMDBlYTA4NjI5OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.x_EaVXQkg1_plk0NVSBnoNUl4QlGytdeO613nXIsP3w",
+		},
+	})
+		.then((response) => response.json())
+		.then((data) => sendMessage("fetch", data, "content-script"))
+		.catch((error) => console.error(error))
+	return true // Indicates that sendResponse will be called asynchronously
+})
+onMessage("fullscreen", async (message: { sender: any }) => {
+	const { sender } = message
+	browser.windows.update(sender.tab.windowId, { state: "fullscreen" })
+})
+onMessage("exitFullscreen", async (message: { sender: any }) => {
+	const { sender } = message
+	browser.windows.update(sender.tab.windowId, { state: "normal" })
+})
+onMessage("setBadgeText", async (message: { sender: any; data: { text: string } }) => {
+	const { sender, data } = message
+	setBadgeText(data.text, sender.tab.id)
+})
+onMessage("increaseBadge", async (message: { sender: any }) => {
+	const { sender } = message
+	increaseBadge(sender.tab.id)
+})
+onMessage("resetBadge", async (message: { sender: any }) => {
+	const { sender } = message
+	if (Badges[sender.tab.id]) delete Badges[sender.tab.id]
+	browser.browserAction.setBadgeText({ text: "", tabId: sender.tab.id })
+})
+
+async function startMobile() {
+	await checkStoreReady(settings)
+	ChangeUserAgent()
+}
+
+if (isMobile) {
+	startMobile()
+}
+const newUa = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0 streamingEnhanced"
+function ReplaceUserAgent(details: any) {
+	if (settings.value.Video.userAgent) {
+		for (const header of details.requestHeaders) {
+			if (header.name === "User-Agent") {
+				header.value = newUa
+				break
+			}
+		}
+	}
+	return { requestHeaders: details.requestHeaders }
+}
+
+function ChangeUserAgent() {
+	browser.webRequest.onBeforeSendHeaders.addListener(
+		ReplaceUserAgent,
+		{
+			urls: [
+				"*://*.disneyplus.com/*",
+				"*://*.starplus.com/*",
+				"*://*.max.com/*",
+				"*://*.hbomax.com/*",
+				// these are only the prime video urls
+				"*://*.primevideo.com/*",
+				"*://*.amazon.com/gp/video/*",
+				"*://*.amazon.co.jp/gp/video/*",
+				"*://*.amazon.de/gp/video/*",
+				"*://*.amazon.co.uk/gp/video/*",
+			],
+		},
+		["blocking", "requestHeaders"],
+	)
+}
