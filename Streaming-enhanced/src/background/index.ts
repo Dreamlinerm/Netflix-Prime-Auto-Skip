4d3fb242971f16c9d0fd4d9a9bbab95c7115c4af
Marvin Alexander Krebber <marvinkrebber@yahoo.de>
2025-01-11 16:41:45 +0100

    finished install page

--- Streaming-enhanced/src/background/index.ts
+++ Streaming-enhanced/src/background/index.ts
@@ -19,7 +19,7 @@ chrome.runtime.onInstalled.addListener(async (opt) => {
 		chrome.tabs.create({
 			active: true,
 			// url: chrome.runtime.getURL("src/ui/options-page/index.html#/options-page"),
-			url: chrome.runtime.getURL("/src/ui/options-page/index.html#/options-page/Backup"),
+			url: chrome.runtime.getURL("src/ui/options-page/index.html#/options-page/install"),
 		})
 	}
 })
--- Streaming-enhanced/src/ui/options-page/pages/install.vue
+++ Streaming-enhanced/src/ui/options-page/pages/install.vue
@@ -1,17 +1,59 @@
 <script setup lang="ts">
-const displayName = __DISPLAY_NAME__
-// const version = __VERSION__
+const optionsStore = useOptionsStore()
+const { settings } = storeToRefs(optionsStore)
+const optionalSettings = computed({
+	get: () => settings.value.Amazon.filterPaid && settings.value.Video.showYear,
+	set: (value) => {
+		settings.value.Amazon.filterPaid = value
+		settings.value.Video.showYear = value
+	},
+})
 </script>
 
 <template>
 	<div class="w-full h-full flex flex-col justify-center items-center text-center md:my-12">
 		<h1>🎉 Installed! 🚀</h1>
 		<p>
-			Thank you for installing {{ displayName }}! ❤️
+			Thank you for installing Streaming enhanced! ❤️
 			<br />
-			Now you can close this tab and start using the extension.
+			Turn on all optional features:
+			<Switch
+				v-model="optionalSettings"
+				class="ml-auto"
+			></Switch>
 		</p>
 	</div>
+	<div class="py-1 m-0 flex">
+		<p>{{ $t("filterPaidSwitch") }}</p>
+		<Switch
+			v-model="settings.Amazon.filterPaid"
+			class="ml-auto"
+		></Switch>
+	</div>
+	<p class="description">
+		{{ $t("defaultfilterPaidDescription") }}
+	</p>
+	<hr />
+	<div class="py-1 m-0 flex">
+		<p>{{ $t("showYearSwitch") }}</p>
+		<Switch
+			v-model="settings.Video.showYear"
+			class="ml-auto"
+		></Switch>
+	</div>
+	<p class="description">
+		{{ $t("showYearDescription") }}
+	</p>
+	<hr />
+	<div class="py-1 m-0 flex">
+		<p>{{ $t("epilepsySwitch") }}</p>
+		<Switch
+			v-model="settings.Video.epilepsy"
+			class="ml-auto"
+		></Switch>
+	</div>
+	<p class="description">
+		{{ $t("epilepsyDescription") }}
+	</p>
+	<hr />
 </template>
-
-<style lang="scss" scoped></style>
