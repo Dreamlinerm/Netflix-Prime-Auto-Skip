8b3c3a9dfaac65bea430ac87083d842e42338740
Marvin Alexander Krebber <marvinkrebber@yahoo.de>
2025-01-11 12:01:59 +0100

    chrome name

--- Streaming-enhanced/manifest.chrome.config.ts
+++ Streaming-enhanced/manifest.chrome.config.ts
@@ -1,8 +1,11 @@
 import { defineManifest } from "@crxjs/vite-plugin"
 import ManifestConfig from "./manifest.config"
+import packageJson from "./package.json" with { type: "json" }
 
+const { name, displayName } = packageJson
 // @ts-expect-error ManifestConfig provides all required fields
 export default defineManifest((env) => ({
 	...ManifestConfig,
+	name: env.mode === "staging" ? `[INTERNAL] ${name}` : displayName.replace(":", "") || name,
 	key: env["CHROME_ADDON_KEY"],
 }))
