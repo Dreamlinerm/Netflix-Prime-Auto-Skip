3d680d511540bb111be89d92833ee728eae46d77
Marvin Alexander Krebber <marvinkrebber@yahoo.de>
2025-01-11 22:48:44 +0100

    key: env[

--- Streaming-enhanced/manifest.chrome.config.ts
+++ Streaming-enhanced/manifest.chrome.config.ts
@@ -6,5 +6,7 @@ const { name, displayName } = packageJson
 // @ts-expect-error ManifestConfig provides all required fields
 export default defineManifest((env) => ({
 	...ManifestConfig,
-	key: env["CHROME_ADDON_KEY"],
+	key: env[
+		"MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAnNxyk3xv8ExvmREOwq7387UYsKbCRQc+Lg0PXlAwERWEAn5zoXpIto4IJvsvFvVl0n9VfkRf37BNDQ+9mElupj1rTcqb7SEwqqZbTNUUoXVfsHdRCkmofmjct3o5O18BGK78ZtSXIICZDCNhiIhoTox1ZtsMQLx23j9EBCjRR4+PXofuK7Y4bDZQznKUvrnaddRlifLTYPHE+MN08SUpC6l0OUdGCcKJHDUAKsDIbmvL9KtuQntkZBDUqrMud/Yw3k7zgopd7vRSTvpICqaH0n9i1u3i6uaMRBI/czr35w/tQjhjXimJh9CgQh+F74Yn6JzLtMAB8qe1dp5NEL0xEQIDAQAB"
+	],
 }))
