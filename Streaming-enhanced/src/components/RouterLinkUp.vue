acbb28e49bdf213de09c49a6ef1915c20ad8885a
Marvin Alexander Krebber <marvinkrebber@yahoo.de>
2025-01-12 13:08:09 +0100

    fix back button

--- Streaming-enhanced/src/components/RouterLinkUp.vue
+++ Streaming-enhanced/src/components/RouterLinkUp.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
 const router = useRouter()
+const route = useRoute()
+function back() {
+	const currentPath = route.fullPath
+	const previousPath = router.options.history.state.back
+	if (previousPath != null && currentPath !== previousPath) {
+		router.back()
+	} else {
+		router.push("/action-popup")
+	}
+}
 </script>
 
 <template>
 	<button
 		class="btn btn-sm my-4"
-		@click="router.back()"
+		@click="back"
 	>
 		<i-ph-arrow-left />
 	</button>
