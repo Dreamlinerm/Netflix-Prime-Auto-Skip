4debb8198235811aa5a55d6cc8184ed1f6f1b1f4
Marvin Alexander Krebber <marvinkrebber@yahoo.de>
2025-01-12 20:58:39 +0100

    refactor vue3 branch

--- scripts/hourstxtToJSON.ts
+++ scripts/hourstxtToJSON.ts
@@ -6,8 +6,12 @@ import path from "path"
 // open AuthorHours.txt and write time into JSON file
 
 const authorHoursFilePath = "authorHours.json"
-const hours = fs.readFileSync(authorHoursFilePath, "utf8")
-const json = { time: hours.split("\n")[0].split(": ")[1] }
+const authorHoursFile = fs.readFileSync(authorHoursFilePath, "utf8")
+const time = authorHoursFile.split("\n")[0].split(": ")[1]
+const hours = parseInt(time.split("h")[0]) + 67 // refactor vue3 branch
+const minutes = time.split("h")[1].split("m")[0]
+const seconds = time.split("h")[1].split("m")[1].split("s")[0]
+const json = { time: hours + "h" + minutes + "m" + seconds + "s" }
 
 fs.writeFile(authorHoursFilePath, JSON.stringify(json), "utf8", function (err) {
 	if (err) return console.log(err)
