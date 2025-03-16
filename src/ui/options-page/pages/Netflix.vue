c8cbfd902d05e98bd3ba7e2d0301269ead65bc17
Marvin Alexander Krebber <marvinkrebber@yahoo.de>
2025-03-16 14:11:51 +0100

    Blocked Titles

--- src/ui/options-page/pages/Netflix.vue
+++ src/ui/options-page/pages/Netflix.vue
@@ -2,13 +2,40 @@
 const hideTitlesStore = useHideTitlesStore()
 const { hideTitles } = storeToRefs(hideTitlesStore)
 console.log(hideTitles.value)
+function removeTitle(title: string) {
+	delete hideTitles.value[title]
+}
+function removeAllTitles() {
+	hideTitles.value = {}
+}
 </script>
 <template>
 	<SharedPagesNetflix advanced-settings></SharedPagesNetflix>
-	<div
-		v-for="(title, index) in hideTitles"
-		:key="index"
-	>
-		{{ title }}
+	<hr />
+	<div class="flex">
+		<p>Blocked Titles:</p>
+		<button
+			class="btn btn-sm btn-error ml-auto"
+			@click="removeAllTitles"
+		>
+			Remove all Blocked Titles
+		</button>
+	</div>
+	<hr />
+	<p>Blocked Titles List:</p>
+	<div class="h-96 w-40 overflow-y-auto">
+		<div
+			v-for="(title, index) in Object.keys(hideTitles)"
+			:key="index"
+			class="flex"
+		>
+			{{ title }}
+			<button
+				class="btn btn-sm btn-error ml-auto"
+				@click="removeTitle(title)"
+			>
+				Remove
+			</button>
+		</div>
 	</div>
 </template>
