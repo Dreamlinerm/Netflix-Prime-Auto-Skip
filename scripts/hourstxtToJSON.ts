ad8f4ad8c157e4f9b7d26d9ff097ea574a999c10
Marvin Alexander Krebber <marvinkrebber@yahoo.de>
2025-03-16 17:03:44 +0100

    hours

--- scripts/hourstxtToJSON.ts
+++ scripts/hourstxtToJSON.ts
@@ -8,7 +8,7 @@ import path from "path"
 const authorHoursFilePath = "authorHours.json"
 const authorHoursFile = fs.readFileSync(authorHoursFilePath, "utf8")
 const time = authorHoursFile.split("\n")[0].split(": ")[1]
-const hours = parseInt(time.split("h")[0]) + 67 // refactor vue3 branch
+const hours = parseInt(time.split("h")[0]) + 67 + 10 // refactor vue3 branch, hideTitles branch
 const minutes = time.split("h")[1].split("m")[0]
 const seconds = time.split("h")[1].split("m")[1].split("s")[0]
 const json = { time: hours + "h" + minutes + "m" + seconds + "s" }
