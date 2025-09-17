577b29f6e60cd04055f1a8d34cfefb3a74d92357
Marvin Alexander Krebber <marvinkrebber@yahoo.de>
2025-09-17 07:48:31 +0200

    descriptionShort

--- manifest.chrome.config.ts
+++ manifest.chrome.config.ts
@@ -2,12 +2,13 @@ import { defineManifest } from "@crxjs/vite-plugin"
 import ManifestConfig from "./manifest.config"
 // import packageJson from "./package.json" with { type: "json" }
 
-// const { name, displayNameShort, descriptionShort } = packageJsonsss
+// displayNameShort,
+const { name, descriptionShort } = packageJsonsss
 // @ts-expect-error ManifestConfig provides all required fields
 export default defineManifest((env) => ({
 	...ManifestConfig,
 	// name: env.mode === "staging" ? `[INTERNAL] ${name}` : displayNameShort || name,
-	// description: descriptionShort,
+	description: descriptionShort,
 	author: {
 		email: "marvinkrebber@yahoo.de",
 	},
